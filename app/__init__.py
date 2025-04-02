490d4ebc579119274e0dbbc6435b23249ce1f1f1
Jobrunlarsen <jobrunlarsen@gmail.com>
2025-04-02 20:38:10 +0200

    First sucsessfull transfer to new page

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ from app.about.routes import about
 from app.home.routes import home
 from app.methodology.routes import methodology
 from app.calculator.routes import calculator
+from app.users.routes import users
 
 application.register_blueprint(about)
 application.register_blueprint(home)
 application.register_blueprint(methodology)
 application.register_blueprint(calculator)
+application.register_blueprint(users)
