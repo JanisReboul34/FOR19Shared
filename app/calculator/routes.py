27493569576bbaba889f6c3d1ee06f306b4840e9
Jobrunlarsen <jobrunlarsen@gmail.com>
2025-04-06 15:48:28 +0200

    Somthing

--- app/calculator/routes.py
+++ app/calculator/routes.py
@@ -1,6 +1,4 @@
 from flask import render_template, Blueprint
-from app.models import Transport #Need this line for the databases to work:) (I think?)
-#Han gjør noe mer her 10c 6.43, men tror ikke det er nødvendig for login/logout
 
 calculator = Blueprint('calculator', __name__)
 
